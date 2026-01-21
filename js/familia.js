f384d1ab91209c549355b5e6c7a3c371772abc64
Luiz <luiztena1@gmail.com>
2026-01-21 11:39:36 -0300

    Feat:Pagina das familias criadas

--- js/familia.js
+++ js/familia.js
@@ -2,6 +2,10 @@ async function carregarFamilia() {
   const params = new URLSearchParams(window.location.search);
   const id = params.get("id");
 
+  console.log("ID DA URL:", id);
+  
+
+
   if (!id) {
     window.location.href = "../404.html";
     return;
@@ -12,23 +16,34 @@ async function carregarFamilia() {
     const familias = await res.json();
 
     const familia = familias[id];
-
+  console.log("FAMILIAS JSON:", familias);
+  console.log("FAMILIA SELECIONADA:", familias[id]);
     if (!familia) {
       window.location.href = "../404.html";
       return;
     }
 
+    // ===== HEADER =====
     document.getElementById("familia-nome").textContent = familia.name;
-    document.getElementById("familia-descricao").textContent = familia.descricao;
-    
-    const img = document.getElementById("familia-imagem");
-    img.src = familia.image;
-    img.alt = familia.name;
+    document.getElementById("familia-descricao").textContent =
+      familia.descricao || "";
 
-  } catch (e) {
-    console.error(e);
+    // ===== FICHA BOTÂNICA (AUTOMÁTICA) =====
+    const ul = document.querySelector("#ficha-botanica ul");
+    ul.innerHTML = "";
+
+    for (const [label, value] of Object.entries(familia.ficha)) {
+      const li = document.createElement("li");
+      li.innerHTML = `<strong>${label}:</strong> ${value}`;
+      ul.appendChild(li);
+    }
+
+  } catch (erro) {
+    console.error(erro);
     window.location.href = "../404.html";
   }
 }
+
+const ul = document.querySelector("#ficha-botanica ul");
 
 document.addEventListener("DOMContentLoaded", carregarFamilia);
